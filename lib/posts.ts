7092fcdd4ea22c3128f55f7490077bb053078833
bmurillo3 <bmurillo3@students.avc.edu>
2026-03-25 11:58:53 -0700

    fixing an error

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,6 @@
 //hold and renders my data, cahngeing some of its content
 // ex - contect variable is changed t the actual content we see on the websites
-/export type Post = {
+export type Post = {
   id: string;
   title: string;
   date: string;
